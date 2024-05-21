4c9c8ac3ef44c3ba045e52460265764c35ff987f
Miruuuuuuu <abdullah561607@gmail.com>
2024-05-21 10:23:42 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -72,10 +72,32 @@ class Block
         // Utility method to get current timestamp (you can implement your own)
         string getCurrentTimestamp() 
         {
-             /* TODO
-             to implement this function go through the experimantation.cpp file
-              */ 
-        return ""; 
+            
+    /* GETTING THE CURRENT TIMESTAMP USING CTIME*/
+
+    // Get the current time in seconds since epoch
+    time_t currentTime = time(nullptr);
+    
+    // Convert the time to a local time struct
+    tm* localTime = localtime(&currentTime);
+    
+    // Output the date and time
+    cout << "Current date and time: ";
+    // Output the year
+    cout << 1900 + localTime->tm_year << "-";
+    // Output the month
+    cout << 1 + localTime->tm_mon << "-";
+    // Output the day
+    cout << localTime->tm_mday << " ";
+    // Output the hour
+    cout << localTime->tm_hour << ":";
+    // Output the minute
+    cout << localTime->tm_min << ":";
+    // Output the second
+    cout << localTime->tm_sec << std::endl;
+
+    return 0;
+}
         }
 
         // Method to calculate block hash (you can implement your own)
